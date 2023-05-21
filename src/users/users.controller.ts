fd34e2ee7560f38288921231cd978f1ee68ca731
gyucode <gyucode@gmail.com>
2023-05-21 14:29:27 +0900

    [fix] edit controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,31 +1,42 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Header, Redirect, Query } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
+import { VerifyEmailDto } from './dto/verify-email.dto';
+import { UserLoginDto } from './dto/user-login.dto';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post()
-  create(@Body() createUserDto: CreateUserDto) {
+  async create(@Body() createUserDto: CreateUserDto) {
+    console.log(createUserDto)
     return this.usersService.create(createUserDto);
   }
 
+  @Post('/email-verify')
+  async verifyEmail(@Query() dto: VerifyEmailDto): Promise<string> {
+    console.log(dto)
+    return;
+  }
+  
+  @Post('/login')
+  async login(@Body() dto: UserLoginDto ): Promise<string>{
+    console.log(dto)
+    return;
+  }
   @Get()
   findAll() {
     return this.usersService.findAll();
   }
 
+  // @Header('Custom', 'Test Header')
+  @Redirect('https://nestjs.com', 301)
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.usersService.findOne(+id);
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-    return this.usersService.update(+id, updateUserDto);
-  }
 
   @Delete(':id')
   remove(@Param('id') id: string) {
